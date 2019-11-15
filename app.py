7b49bbeacef4d35a23bbe042e3e5ab5eee6496d2
Khizar Malik <k.malik@se17.qmul.ac.uk>
2019-11-15 00:05:24 +0000

    New functionality added - refresh token and blacklisting

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from flask_jwt_extended import JWTManager
-from resources.user import User, UserRegistry, UserLogin
+from resources.user import User, UserRegistry, UserLogin, TokenRefresh, UserLogout
 from database import db
-
+from blacklisting import BLACKLIST
 app = Flask(__name__)
 
 api = Api(app)
@@ -13,6 +13,8 @@ jwt = JWTManager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = "example"
 
 class Homepage(Resource):
@@ -20,11 +22,49 @@ class Homepage(Resource):
 	def get(self):
 		return "<h1> Service working </h1>"
 
+@jwt.expired_token_loader
+def expired_token_callback():
+	return jsonify({
+		"description" : "token is expired",
+		"error"  : "Token expired"
+		}), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+	return jsonify({ "description" : "Passed access token is invalid", "error" : "Invalid token"})
+
+@jwt.unauthorized_loader
+def unauthorized_callback(error):
+	return jsonify({
+		"description" : "Unauthorised access - login is required",
+		"error" : "unauthorised access"
+	})
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+	return decrypted_token['jti'] in BLACKLIST
+ 
+@jwt.needs_fresh_token_loader
+def fresh_token_callback():
+	return jsonify({
+		"description" : "Fresh access token is required - please login",
+		"error" : "Login required"
+	})
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+	return jsonify({
+		"description" : "User has logout of the api",
+		"error" : "User logout"
+	})
+
+
 api.add_resource(User, "/user")
 api.add_resource(UserRegistry, "/register")
 api.add_resource(UserLogin, "/login")
 api.add_resource(Homepage, "/")
-
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(UserLogout, "/logout")
 
 
 @app.before_first_request
--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,16 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+	create_access_token, 
+	create_refresh_token, 
+	jwt_refresh_token_required,
+	get_jwt_identity,
+	fresh_jwt_required,
+	jwt_required,
+	get_raw_jwt
+)
+from blacklisting import BLACKLIST
 
 _parser = reqparse.RequestParser()
 _parser.add_argument("username", type=str, required=True, help="This field is required")
@@ -9,12 +18,22 @@ _parser.add_argument("password", type=str, required=True, help="This field is re
 
 class User(Resource):
 
-	@classmethod
+	@jwt_required
 	def get(cls):
 		data = _parser.parse_args()
 		username = data['username']
 		user = UserModel.find_by_username(username)
 		if user:
+			return user.json()
+		return {"message" : "user not found"}
+
+	@fresh_jwt_required
+	def put(self):
+		data = _parser.parse_args()
+		username = data['username']
+		user = UserModel.find_by_username(username)
+		if user:
+			user.password = data['password']
 			return user.json()
 		return {"message" : "user not found"}
 
@@ -47,3 +66,23 @@ class UserLogin(Resource):
 				}
 		return {"message" : "Invalid credential"}, 401
 		
+
+
+class TokenRefresh(Resource):
+
+	@jwt_refresh_token_required
+	def post(self):
+		user_id = get_jwt_identity()
+		access_token = create_access_token(identity=user_id, fresh=False)
+		return {"access_token" : access_token}, 200
+	
+
+class UserLogout(Resource):
+	@jwt_required
+	def post(self):
+		access_id = get_raw_jwt()['jti']
+		BLACKLIST.add(access_id)
+		return {"message" : "Logout successful"}
+				
+
+
